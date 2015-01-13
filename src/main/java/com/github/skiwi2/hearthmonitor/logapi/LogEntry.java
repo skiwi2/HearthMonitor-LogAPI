9543fae0e766618fe4d765db85f036143bbd556d
skiwi <frank_skiwi@hotmail.com>
2015-01-13 14:15:20 +0100

    Updated javadoc of LogObject and LogEntry.

--- src/main/java/com/github/skiwi2/hearthmonitor/logapi/LogEntry.java
+++ src/main/java/com/github/skiwi2/hearthmonitor/logapi/LogEntry.java
@@ -3,6 +3,8 @@ package com.github.skiwi2.hearthmonitor.logapi;
 /**
  * Marker interface to denote that a class can be used as an log entry.
  *
+ * A log entry always ranges over at least one full line of the log file, and thus is also a log object.
+ *
  * @author Frank van Heeswijk
  */
 public interface LogEntry extends LogObject { }
--- src/main/java/com/github/skiwi2/hearthmonitor/logapi/LogObject.java
+++ src/main/java/com/github/skiwi2/hearthmonitor/logapi/LogObject.java
@@ -3,6 +3,8 @@ package com.github.skiwi2.hearthmonitor.logapi;
 /**
  * Marker interface to denote that a class can be used as a log object.
  *
+ * A log object can appear anywhere in a log file.
+ *
  * @author Frank van Heeswijk
  */
 public interface LogObject { }
